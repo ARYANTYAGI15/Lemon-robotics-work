d427aeb1f395daae3c5b347646e94d2a13fbe135
ARYANTYAGI15 <aryan.india1515@gmail.com>
2024-10-02 13:13:57 +0530

    Work

---
+++ lemon-frontend-main/src/pages/index.js
@@ -0,0 +1,11 @@
+// src/pages/index.js
+
+export { default as Home } from "./Home";
+export { default as About } from "./About";
+export { default as AllServicePage } from "./AllServicePage/AllServicePage";
+export { default as VirtualCommission } from "./ServiceDetailPages/VirtualCommissioning/VirtualCommission";
+export { default as SoftwareTesting } from "./ServiceDetailPages/SoftwareTesting/SoftwareTesting";
+export { default as Login } from "./Login"; // Your existing Login export
+export { default as Dashboard } from "./Dashboard"; // Export Dashboard component
+export { default as ExpenseSheet } from "./ExpenseSheet"; // Export ExpenseSheet component
+export { default as TimeSheet } from "./TimeSheet"; // Export TimeSheet component

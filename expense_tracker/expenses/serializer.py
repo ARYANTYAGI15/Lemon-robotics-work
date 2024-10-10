60b31a7b7f42e09423d74657957df07a21886321
ARYANTYAGI15 <aryan.india1515@gmail.com>
2024-10-10 18:11:44 +0530

    removed

--- expense_tracker/expenses/serializer.py
+++
@@ -1,9 +0,0 @@
-# expenses/serializers.py
-
-from rest_framework import serializers
-from .models import Expense
-
-class ExpenseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Expense
-        fields = '__all__'  # Serialize all fields

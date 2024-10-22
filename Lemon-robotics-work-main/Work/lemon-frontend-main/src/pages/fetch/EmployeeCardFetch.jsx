286c6b6bbf58d140b875ff7e0525662d8cb403f3
ARYANTYAGI15 <aryan.india1515@gmail.com>
2024-10-22 20:25:51 +0530

    Expense sheet api connected

--- Lemon-robotics-work-main/Work/lemon-frontend-main/src/pages/fetch/EmployeeCardFetch.jsx
+++ Lemon-robotics-work-main/Work/lemon-frontend-main/src/pages/fetch/EmployeeCardFetch.jsx
@@ -36,7 +36,7 @@ const EmployeecardFetch = () => {
       <h2>{user?.map((u) => u.first_name)}</h2>
       <EmployeeCardRender
         name={user?.map((u) => u.first_name)}
-        empid={user?.[0].user_id}
+        empid={user?.[0].empid}
         phone={user?.[0].phone}
         designation={user?.[0].designation}
         department={user?.[0].department}
